c99c88fc99a7fe284bf869afffb456cf9e31b025
Tuan Anh Le <tuananhle@Tuans-iMac.attlocal.net>
2021-07-08 22:41:10 -0400

    added routes for messages and users with pg relationships

--- routes/messages.js
+++ routes/messages.js
@@ -10,21 +10,18 @@ const ExpressError = require("../expressError");
 
 router.get("/:id", async function (req, res, next) {
   try {
-    const result = await db.query(
-          `SELECT m.id, m.msg, t.tag
-             FROM messages AS m
-               LEFT JOIN messages_tags AS mt 
-                 ON m.id = mt.message_id
-               LEFT JOIN tags AS t ON mt.tag_code = t.code
-             WHERE m.id = $1;`,
-        [req.params.id]);
-
-    let { id, msg } = result.rows[0];
-    let tags = result.rows.map(r => r.tag);
-
-    return res.json({ id, msg, tags });
+    const results = await db.query(`SELECT m.id, m.msg, t.tag
+    FROM messages m 
+    LEFT JOIN messages_tags mt ON m.id = mt.message_id
+    LEFT JOIN tags t ON t.code = mt.tag_code
+    WHERE m.id = $1`, [req.params.id]);
+    if (results.rows.length == 0){
+      throw new ExpressError("Message not found", 404);
+    }
+    const {id, msg} = results.rows[0];
+    const tags = results.rows.map(r=>r.tag);
+    return res.json({message: {id, msg, tags}});
   }
-
   catch (err) {
     return next(err);
   }
@@ -33,21 +30,6 @@ router.get("/:id", async function (req, res, next) {
 
 /** Update message: {msg} => {id, user_id, msg} */
 
-router.put("/:id", async function (req, res, next) {
-  try {
-    const result = await db.query(
-          `UPDATE messages SET msg=$1 WHERE id = $2
-           RETURNING id, user_id, msg`,
-        [req.body.msg, req.params.id]);
-
-    return res.json(result.rows[0]);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-// end
 
 /** Update message #2: {msg} => {id, user_id, msg} */
 
@@ -76,25 +58,20 @@ router.put("/v2/:id", async function (req, res, next) {
 // end
 
 /** Update message #3: {msg} => {id, user_id, msg} */
-
-router.put("/v3/:id", async function (req, res, next) {
+router.patch('/:id', async (req, res, next)=>{
   try {
-    const result = await db.query(
-          `UPDATE messages SET msg=$1 WHERE id = $2
-           RETURNING id, user_id, msg`,
-        [req.body.msg, req.params.id]);
-
-    if (result.rows.length === 0) {
-      throw new ExpressError("No such message!", 404);
+    const results = await db.query(`
+      UPDATE messages SET msg = $1 WHERE id = $2
+    RETURNING id, user_id, msg`, [req.body.msg, req.params.id]);
+    if (results.rows.length==0){
+      throw new ExpressError("Message not found", 404);
     }
-
-    return res.json(result.rows[0]);
-  }
-
-  catch (err) {
+    return res.json(results.rows[0]);
+  } catch (err) {
     return next(err);
   }
-});
+
+})
 // end
 
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 const db = require("../db");
 const express = require("express");
+const ExpressError = require("../expressError");
 const router = express.Router();
 
 
@@ -21,27 +22,22 @@ router.get("/", async function (req, res, next) {
 });
 
 /** Get user: {name, type, messages: [{msg, msg}]} */
-
-router.get("/:id", async function (req, res, next) {
+router.get("/:id", async(req, res, next)=>{
   try {
-    const userRes = await db.query(
-          `SELECT name, type FROM users WHERE id=$1`,
-        [req.params.id]);
-
-    const messagesRes = await db.query(
-          `SELECT id, msg FROM messages 
-             WHERE user_id = $1`,
-        [req.params.id]);
-
-    const user = userRes.rows[0];
-    user.messages = messagesRes.rows;
-    return res.json(user);
-  }
-
-  catch (err) {
+    const userResult = await db.query(`SELECT name, type 
+    FROM users WHERE id = $1`, [req.params.id]);
+    const messageResult = await db.query(`SELECT id, msg
+    FROM messages WHERE user_id = $1`,[req.params.id]);
+    if (results.rows.length==0){
+      throw new ExpressError("User not found", 404);
+    }
+    const user = userResult.rows[0];
+    user.messages = messageResult.rows;
+    return res.json({user: user})
+  } catch (err) {
     return next(err);
   }
-});
+})
 
 
 module.exports = router;
